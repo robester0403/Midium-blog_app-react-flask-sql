b6e004d85b779fe04779f9682f82d1cdf04b10f6
robester0403 <robkso@gmail.com>
2022-11-23 05:06:09 -0500

    Removed Web

--- web/src/context/ZustandStore.js
+++
@@ -1,7 +0,0 @@
-import create from "zustand";
-
-export const useArticleStore = create((set) => ({
-  allArticles: {},
-  hydrateAllArticles: (articles) => set({ allArticles: articles }),
-  clearAllArticles: () => set({ allArticles: {} }),
-}));
--- web/src/views/Antimedium.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Antimedium = () => {
-  return <div>Antimedium</div>;
-};
-
-export default Antimedium;
